6231a6c05a07c6ff0de72e383f15bd7e95a69162
MalyasovDV <pudge24@mail.ru>
2023-07-08 00:10:26 +0700

    task2 done

--- program.java
+++ program.java
@@ -18,6 +18,17 @@ public class program {
         }
     }
 
+    public static void removeNumbers(ArrayList<String> list) {
+        for (Iterator<String> iterator = list.iterator(); iterator.hasNext();) {
+            String nextString = iterator.next();
+            try {
+                Integer.parseInt(nextString);
+                iterator.remove();
+            }
+            catch (NumberFormatException e) {}
+        }
+    }
+
     public static void main(String[] args) {
         ArrayList<Integer> array = new ArrayList<>();
         fillList(array, 10);
@@ -25,5 +36,11 @@ public class program {
         System.out.println();
         removeOddNumbers(array);
         System.out.println(array);
+
+        ArrayList<String> list = new ArrayList<>(List.of("string", "s", "5", "10", "-12", "my_value"));
+        System.out.println(list);
+        System.out.println();
+        removeNumbers(list);
+        System.out.println(list);
     }
 }
